6dc408e7f6b48a36154a0345b92ae1f30690eeaf
Gaëtan Pascreau <62143619+GaetanPascreau@users.noreply.github.com>
2023-06-04 13:47:29 +0200

    Add Identity as a separate service

---
+++ CHidentityService/Data/ApplicationIdentityDbContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace CHidentityService.Data
+{
+    public class ApplicationIdentityDbContext : IdentityDbContext<Patient>
+    {
+        public ApplicationIdentityDbContext(DbContextOptions<ApplicationIdentityDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
---
+++ CHidentityService/Data/Patient.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System.ComponentModel.DataAnnotations;
+
+namespace CHidentityService.Data
+{
+    public class Patient : IdentityUser
+    {
+        [StringLength(50)]
+        public string FName { get; set; }
+
+        [StringLength(50)]
+        public string LName { get; set; }
+
+        [StringLength(255)]
+        public string Address1 { get; set; }
+
+        [StringLength(255)]
+        public string Address2 { get; set; }
+
+        [StringLength(50)]
+        public string City { get; set; }
+
+        [StringLength(10)]
+        public string Postcode { get; set; }
+    }
+}
